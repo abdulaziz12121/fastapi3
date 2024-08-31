e9ac07f301f0f725c2a3f58e219d6a5b56de55b9
abdulaziz <abdulaziz125000f@gmail.com>
2024-09-01 02:30:28 +0300

    Abdulaziz

--- main.py
+++ main.py
@@ -3,36 +3,27 @@ import joblib
 from pydantic import BaseModel
 from typing import List
 
+
 app = FastAPI()
 
-# Load the saved KMeans model and scaler1
+# Load the saved DBSCAN model and scaler
 dbscan_model = joblib.load('DBSCAN_model1.joblib')
-DBSCAN_scaler1 = joblib.load('scaler1.joblib')
-
-app = FastAPI()
 
 class InputFeatures(BaseModel):
-    yellow:float
-    red:float
-    position_encoded:int
-
+    yellow: float
+    red: float
+    position_encoded: int
 
 def preprocess_dbscan(input_features: InputFeatures):
     data = [[input_features.yellow, input_features.red, input_features.position_encoded]]
-    scaled_data = DBSCAN_scaler1.transform(data)
-    return scaled_data
-
+    predictions = dbscan_model.fit_predict(data)
+    return predictions
 
 @app.post("/predict_dbscan")
 async def predict_dbscan(input_features: InputFeatures):
     data = preprocess_dbscan(input_features)
-    y_pred = dbscan_model.fit_predict(data)
-    return {"dbscan_pred": int(y_pred[0])}
+    return {"dbscan_pred": int(data)}
 
-
-    
-
- 
-
-
-
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
